2b13dabdc087f2263d77e53600367913bee8f40e
Lionel Mason <capitalcookhouse@gmail.com>
2022-12-01 14:39:18 -0500

    commit tree 1cb57edf29e799363aeb33b133e61fcf56ddac9e

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+# class Dog
+#     def bark
+#         puts "Woof!"
+#     end
+#     def sit
+#         puts "The Dog is sitting"
+#     end
+# end
+
+# fido = Dog.new
+# fido.bark
+
+# snoopy = Dog.new
+# snoopy.bark
+
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
